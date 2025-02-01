8e73095169b519f057d3ee185ee26cadc60904b5
devicarus <devicarus@icloud.com>
2025-02-01 19:14:09 +0100

    DEBUG mode now affects only console output; changed imports

--- src/logger.ts
+++ src/logger.ts
@@ -1,10 +1,10 @@
-import winston, { Logger, transports, format } from 'winston';
+import { createLogger, Logger, transports, format, config } from 'winston';
 import { DEBUG } from './config';
 
-export default winston.createLogger({
-  level: DEBUG ? 'debug' : 'info',
+export default createLogger({
+  levels: config.syslog.levels,
   format: format.combine(
-		winston.format.timestamp({
+		format.timestamp({
       format: 'YYYY-MM-DD HH:mm:ss'
     }),
     format.prettyPrint(),
@@ -16,11 +16,12 @@ export default winston.createLogger({
     new transports.File({ filename: 'logs/combined.log' }),
     new transports.Console({
       format: format.combine(
-        winston.format.colorize({
+        format.colorize({
           level: true
         }),
         format.printf(info => `${info.timestamp} [${info.level}]: ${info.message}`)
       ),
+      level: DEBUG ? 'debug' : 'info'
     })
   ],
 }) satisfies Logger;
